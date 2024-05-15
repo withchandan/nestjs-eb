abe603da3bbc7dd042f280d30395a0c1b6ea1b36
Chandan Vishwakarma <sanyukt1994@gmail.com>
2024-05-15 17:33:10 +0530

    update health api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('health')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getHello(): Record<string, any> {
     return this.appService.getHello();
   }
 }
